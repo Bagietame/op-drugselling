1bca5eea08df111142d7a73c08afff06a053f4a0
Bagieta <75392567+Bagietame@users.noreply.github.com>
2025-10-04 18:37:37 +0200

    Initial commit
    Added core files for OP-DrugSelling, a dynamic drug dealing system for FiveM.
    
    Includes:
    - Ped-based interaction logic (sell to peds, different personalities)
    - Configurable drug types with min/max price ranges
    - Randomized dialogue lines for immersion
    - Corner selling system and ped reactions (flee/refuse)
    - ESX/QBCore/QBox compatibility structure

---
+++ fxmanifest.lua
@@ -0,0 +1,43 @@
+fx_version "cerulean"
+
+description "Drug Selling Script | Fully Compatible with OP-GANGS 2.0!"
+author "OTHERPLANET"
+version '1.0.0'
+lua54 'yes'
+game 'gta5'
+
+ui_page 'web/build/index.html'
+
+shared_scripts {
+	'@ox_lib/init.lua',
+	'framework/shared.lua',
+	'config/MainConfig.lua',
+	'locales/*.lua',
+}
+
+client_scripts {
+	'config/MainConfig.lua',
+	'framework/client/c_framework.lua',
+	'integrations/client/**',
+	'client/**',
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'config/ServerConfig.lua',
+	'framework/server/s_framework.lua',
+	'server/**',
+}
+
+files {
+	'web/build/**/*',
+}
+
+escrow_ignore {
+	'client/**',
+	'config/**',
+	'locales/**',
+	'framework/**',
+	'server/**',
+	'integrations/**'
+}
